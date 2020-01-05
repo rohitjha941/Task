03906d166d7025640f929eb9c99d55057a0a390c
Rohit <rjha@ph.iitr.ac.in>
2020-01-05 21:30:46 +0530

    Updates

---
+++ precache-manifest.6a33335d47ab222a2a53232156ae8346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e07bc0cd5375776d4ee6762f66ce8c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1625844ae78a2878f3b9",
+    "url": "/static/css/main.414ece1f.chunk.css"
+  },
+  {
+    "revision": "7ad3b223ced1fe5fc458",
+    "url": "/static/js/2.594c84a6.chunk.js"
+  },
+  {
+    "revision": "497d7e07705261348096eb742962e0f0",
+    "url": "/static/js/2.594c84a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1625844ae78a2878f3b9",
+    "url": "/static/js/main.a44639e3.chunk.js"
+  },
+  {
+    "revision": "93f173fd0339454aaf8d",
+    "url": "/static/js/runtime-main.293093f1.js"
+  }
+]);
